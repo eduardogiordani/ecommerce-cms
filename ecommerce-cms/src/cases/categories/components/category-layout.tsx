77ed60762c1ebd0f6769649e176acedd0dd57a3a
eduardogiordani <eduardogiordani42@gmail.com>
2025-10-27 22:02:46 -0300

    Aula 27/10/2025

--- ecommerce-cms/src/cases/categories/components/category-layout.tsx
+++ ecommerce-cms/src/cases/categories/components/category-layout.tsx
@@ -1,32 +1,40 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+import { BreadCrumb } from "@/components/layout/bread-crumb";
 import { CategoryDataTable } from "./data-table/category-data-table";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+import { Plus, Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 export function CategoryLayout() {
+
+    const navigate = useNavigate();
+
+    function handleCreate() {
+        navigate('/categories/new');
+    }
+
     return (
-        <div className="flex flex-col py-4 gap-4">
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                    </BreadcrumbItem>
+        <div className="p-4">
 
-                    <BreadcrumbSeparator />
+            <BreadCrumb title="Categorias"/>
 
-                    <BreadcrumbItem>
-                       <BreadcrumbPage>Categorias</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
-
-            <div>
-                <CategoryDataTable />
+            <div className="flex flex-col py-4 gap-4">
+            
+                <div className="flex flex-row justify-end gap-4 my-4">
+                    <InputGroup className="max-w-96">
+                        <InputGroupInput placeholder="Search..." />
+                        <InputGroupAddon>
+                            <Search />
+                        </InputGroupAddon>
+                    </InputGroup>
+                    <Button onClick={handleCreate}>
+                        <Plus/>
+                        Adicionar
+                    </Button>
+                </div> 
+                <div>
+                    <CategoryDataTable />
+                </div>            
             </div>
         </div>
     );
